a7a3c4af25a67df897b26cff18ca5056d323eee9
Ruslan Sabirov <rsabirov22@yandex.com>
2022-10-03 17:45:23 +0600

    bugfix: create user name required

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.post('/api/signin', celebrate({
 }), login);
 router.post('/api/signup', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
